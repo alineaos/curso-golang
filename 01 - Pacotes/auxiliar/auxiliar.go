900afafa4e70b4879f561aa4cd7a94e7e843e72c
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-09 21:08:27 -0300

    Aula 05 - Pacotes

---
+++ 01 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,10 @@
+package auxiliar
+
+import "fmt"
+
+// É importante funções do Go terem comentários acima delas
+// Escrever registra uma mensagem na tela
+func Escrever(){
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
+}
