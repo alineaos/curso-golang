d1c26010c6fb0b408fcca9d9b209839da06439b2
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-11 17:29:11 -0300

    Aula 14 - Arrays Internos

--- 09 - Arrays e Slices/arrays-e-slices.go
+++ 09 - Arrays e Slices/arrays-e-slices.go
@@ -11,7 +11,7 @@ func main() {
 
 	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
 	fmt.Println(array2)
-	
+
 	array3 := [...]int{1, 2, 3, 4, 5}
 	fmt.Println(array3)
 
@@ -26,4 +26,21 @@ func main() {
 
 	array2[1] = "Posição Alterada"
 	fmt.Println(slice2)
+
+	// ARRAYS INTERNOS
+	fmt.Println("--------------------")
+	slice3 := make([]float32, 10, 11)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
