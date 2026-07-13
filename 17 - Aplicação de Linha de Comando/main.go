f88c84421909f77adefbadee0fab8815d1a34925
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-13 20:13:19 -0300

    Aulas 31 a 33 - Mini Aplicação de Linha de Comando
    - Estruturando a Aplicação de Linha de Comando
    - Criando a Funcionalidade de Busca por IP e Servidor

---
+++ 17 - Aplicação de Linha de Comando/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	aplicacao := app.Gerar()
+
+	// erro := aplicacao.Run(os.Args)
+	// if erro != nil{
+	// 	log.Fatal(erro)
+	// }
+
+	if erro := aplicacao.Run(os.Args); erro != nil{
+		log.Fatal(erro)
+	}
+}
